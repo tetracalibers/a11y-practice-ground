957b9df537009fa4db6c4f11ffe0289fe6c24464
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-11-19 14:44:17 +0900

    yaml,mdx,svelteを追加したastroプロジェクト作成

---
+++ astro.config.mjs
@@ -0,0 +1,26 @@
+// @ts-check
+
+import { defineConfig } from "astro/config"
+import svelte from "@astrojs/svelte"
+import mdx from "@astrojs/mdx"
+import { resolve } from "node:path"
+import yaml from "@rollup/plugin-yaml"
+const __dirname = new URL(".", import.meta.url).pathname
+
+export default defineConfig(
+  /** @type {import('astro').AstroUserConfig} */
+  {
+    vite: {
+      plugins: [yaml()],
+      resolve: {
+        alias: {
+          "@": resolve(__dirname, "src"),
+        },
+      },
+      ssr: {
+        noExternal: ["@acab/reset.css"],
+      },
+    },
+    integrations: [svelte(), mdx()],
+  },
+)
