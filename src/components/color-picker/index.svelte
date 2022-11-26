a191220bcddac105c6d0cea024782e49bc57f577
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-11-26 20:27:07 +0900

    [ColorPicker] 背景を現在の色のプレビュー化

---
+++ src/components/color-picker/index.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import ColorPicker from "./color-picker.svelte"
+
+  $: color = "#ECC5FB"
+  const onChange = (newColor: string) => {
+    color = newColor
+  }
+</script>
+
+<div class="preview-container" style:--bg={color}>
+  <ColorPicker {color} {onChange} />
+</div>
+
+<style>
+  .preview-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+    background-color: var(--bg);
+  }
+</style>
